0ff534f0ee7566ba3449cf188838adc916adc787
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-02-04 23:32:11 +0200

    project setup

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:rickandmorty_app/app_router.dart';
+
+void main() {
+  runApp(RickAndMortyApp(appRouter: AppRouter()));
+}
+
+class RickAndMortyApp extends StatelessWidget {
+  final AppRouter appRouter;
+
+  const RickAndMortyApp({Key? key, required this.appRouter}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: appRouter.generateRoute,
+    );
+  }
+}
