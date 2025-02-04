c634838e125913c31c9fbc6e3bc0d721add9b0e0
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-02-04 23:46:02 +0200

    created characaters details screen class and route

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:rickandmorty_app/constants/Strings.dart';
+import 'package:rickandmorty_app/presentation/screens/characters_details._screen.dart';
 import 'package:rickandmorty_app/presentation/screens/characters_screen.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
+      case CharactersScreen:
         return MaterialPageRoute(builder: (_) => CharactersScreen());
+
+        case charactersDetailsScreen:
+        return MaterialPageRoute(builder: (_) => CharactersDetailsScreen());
+        
     }
+    
   }
 }
---
+++ lib/constants/strings.dart
@@ -0,0 +1,3 @@
+const String baseUrl = 'https://rickandmortyapi.com/api/';
+const String charactersScreen = '/';
+const String charactersDetailsScreen = '/characters_details';
