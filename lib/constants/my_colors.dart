bdd5bb4e7401ebd9e7929d80d46ba4fc6be649c5
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-02-04 23:49:18 +0200

    constants

---
+++ lib/constants/my_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  
+static const Color Yellow = Color(0xffFFC107);
+static const Color grey = Color(0xff343A40);
+static const Color white = Color(0xffE18EB);
+
+
+}
