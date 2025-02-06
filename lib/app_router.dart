b11c528468d4c49aecf827e5889d9e2c03438ab7
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-02-06 21:38:51 +0200

    created BlocProvider

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rickandmorty_app/business_logic/cubit/characters_cubit.dart';
 import 'package:rickandmorty_app/constants/Strings.dart';
+import 'package:rickandmorty_app/data/repository/characters_repository.dart';
+import 'package:rickandmorty_app/data/web_services/characters_web_services.dart';
 import 'package:rickandmorty_app/presentation/screens/characters_details._screen.dart';
 import 'package:rickandmorty_app/presentation/screens/characters_screen.dart';
 
 class AppRouter {
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepository =
+        CharactersRepository(charactersWebServices: CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case CharactersScreen:
-        return MaterialPageRoute(builder: (_) => CharactersScreen());
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (context) => charactersCubit,
+                  child: const CharactersScreen(),
+                ));
 
-        case charactersDetailsScreen:
-        return MaterialPageRoute(builder: (_) => CharactersDetailsScreen());
-        
+      case charactersDetailsScreen:
+        return MaterialPageRoute(
+            builder: (_) => const CharactersDetailsScreen());
     }
-    
   }
 }
--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:rickandmorty_app/business_logic/cubit/characters_cubit.dart';
+import 'package:rickandmorty_app/constants/my_colors.dart';
+import 'package:rickandmorty_app/data/models/character.dart';
 
-class CharactersScreen extends StatelessWidget {
+class CharactersScreen extends StatefulWidget {
   const CharactersScreen({super.key});
 
   @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+  late List<Character> allCharacters;
+
+  @override
+  void initState() {
+    super.initState();
+    allCharacters =
+        BlocProvider.of<CharactersCubit>(context).getAllCharacters();
+  } // ui asks bloc for data   wakes it up bec it's lazy
+
+
+
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Bavlyyyyy'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: MyColors.Yellow,
+        title: const Text(
+          'Characters',
+          style: TextStyle(color: MyColors.grey),
+        ),
+      ),
+      body: buildBlocWidget() ,
     );
   }
 }
